94185e2ae3685b792d630867e85ccb5c9ff710d3
Mottuqe Brid <mottuqebrid@gmail.com>
2024-08-20 10:01:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <MovieCounter></MovieCounter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+
+function MovieCounter() {
+  const [count, setCount] = useState(0);
+  const handleCLickAdd = () => setCount(count + 1)
+  const handleCLickRemove = () => setCount(count - 1)
+  return (
+    <div>
+      <button onClick={handleCLickAdd}>Add Movie</button>
+      <button onClick={handleCLickRemove}>Remove Movie</button>
+      <h3>Number of Movies: {count}</h3>
+      <MovieDisplay movies={count}></MovieDisplay>
+    </div>
+  )
+}
+
+function MovieDisplay(props) {
+  return (
+    <h4>Movie I have acted: {props.movies}</h4>
+  )
 }
 
 export default App;
